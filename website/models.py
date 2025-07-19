a00a7cba4798d6d3df96748be24dd3900da4d97f
Maki <mcjohndraycabral@gmail.com>
2025-07-19 18:26:16 +0800

    setup: user & note class -> table

--- website/models.py
+++ website/models.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from . import db
+from flask_login import UserMixin
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'firstflaskproj'
-    return app
+
+class Note(db.Model):    
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String())
