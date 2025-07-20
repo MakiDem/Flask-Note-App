c625b7d307742a3e6a66908ae384a8948dacc4c7
Maki <mcjohndraycabral@gmail.com>
2025-07-20 17:02:31 +0800

    add: create db func & db instance(removed from git tracking)

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = 'database.db'
@@ -9,6 +10,7 @@ def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'firstflaskproj'
   app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
   from .view import views
   from .auth import auth
@@ -17,5 +19,13 @@ def create_app():
   app.register_blueprint(auth, url_prefix='/')
 
 
+  from .models import User, Note
+
+  create_database(app)
+
   return app
 
+def create_database(app):
+  if not path.exists('website/' + DB_NAME):
+    with app.app_context():
+      db.create_all()
