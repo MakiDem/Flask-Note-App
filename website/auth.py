0cf4b9711a1eee1b010f65a9a1898fb483adcdc5
Maki <mcjohndraycabral@gmail.com>
2025-07-23 18:12:10 +0800

    add: login manager module

--- website/auth.py
+++ website/auth.py
@@ -27,9 +27,10 @@ def login():
     return render_template('login.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
     logout_user()
-    return redirect(url_for('views.login'))
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
